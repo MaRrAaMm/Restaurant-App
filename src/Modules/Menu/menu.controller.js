ae12c1a7b11f5be0a48943e0a421ab50057256a7
maram <maram@gmail.com>
2025-09-27 19:07:46 +0300

    fix: renamed upload utils and updated imports

--- src/Modules/Menu/menu.controller.js
+++ src/Modules/Menu/menu.controller.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { isAuthenticate } from "../../Middlewares/auth.middleware.js";
 import { isAuthorization } from "../../Middlewares/authorization.middleware.js";
 import { roles } from "../../DB/Models/user.model.js";
-import { cloudUpload, fileValidation } from "../../utils/file uploads/multer cloud.js";
+import { cloudUpload, fileValidation } from "../../Utils/fil-uploads/multer-cloud.js";
 import * as menuValidation from "./menu.validation.js";
 import * as menuService from "./menu.service.js";
 import { asyncHandler } from "../../Utils/error/async-handler.js";
